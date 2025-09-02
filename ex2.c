b935acae8bbf4ae07b8d058b1cb3d14fc899cd5c
elhoucine20 <elhoucinenaitbrahim@gmail.com>
2025-09-02 12:01:36 +0100

    ex2

---
+++ ex2.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main()
+{
+   float const Pi=3.14;
+   int r=10;
+   float v;
+   v=4.0f/3.0f*Pi*r*r*r;
+   printf("le volum d'une sphere est %.2f mL",v); 
+
+    return 0;
+}
