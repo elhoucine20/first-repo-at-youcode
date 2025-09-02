bc9f78bae725eb5ff030a1c2485eaf9bb711807b
elhoucine20 <elhoucinenaitbrahim@gmail.com>
2025-09-02 14:06:41 +0100

    challange 6

---
+++ ex6.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+int x,P;
+printf("saisir la valeur de x :");
+scanf("%d",&x);
+
+P=((((3*x+2)*x-5)*x-1)*x+7)*x -6;
+printf("la valesur du polynome est %d \n",P);
+    
+return 0;
+}
