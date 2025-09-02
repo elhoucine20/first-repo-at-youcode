24824553ece6b6bca45bd260870ce9fa9410986f
elhoucine20 <elhoucinenaitbrahim@gmail.com>
2025-09-02 12:15:53 +0100

    3 demande a lutilisateur dentrer le rayon

--- ex2.c
+++ ex2.c
@@ -2,9 +2,11 @@
 int main()
 {
    float const Pi=3.14;
-   int r=10;
    float v;
-   v=4.0f/3.0f*Pi*r*r*r;
+   float r;
+   printf("saisir le rayon du sphere :");
+   scanf("%f",&r);
+   v=4.0f/3.0f*Pi*r*r*r; //dans v=4/3*Pi*r*r*r : 4/3=1 car les deux valeur 4 et 3 ce sont dest int
    printf("le volum d'une sphere est %.2f mL",v); 
 
     return 0;
