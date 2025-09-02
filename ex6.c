88d9da8eff720300cfd4b3c48b0cee18e1ca3a43
elhoucine20 <elhoucinenaitbrahim@gmail.com>
2025-09-02 14:10:34 +0100

    challange 6

--- ex6.c
+++ ex6.c
@@ -6,7 +6,7 @@ int x,P;
 printf("saisir la valeur de x :");
 scanf("%d",&x);
 
-P=((((3*x+2)*x-5)*x-1)*x+7)*x -6;
+P=((((3*x+2)*x-5)*x-1)*x+7)*x -6; // la methode d'Horner
 printf("la valesur du polynome est %d \n",P);
     
 return 0;
