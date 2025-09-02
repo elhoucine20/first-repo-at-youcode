2bee7edcb0189cac67b11a64806844f4a5d0263b
elhoucine20 <elhoucinenaitbrahim@gmail.com>
2025-09-02 13:45:42 +0100

    chalenge 5

---
+++ ex5.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+int x,P;
+printf("saisir la valeur de x :");
+scanf("%d",&x);
+P=3*(x*x*x*x*x)+2*(x*x*x*x)-5*(x*x*x)-(x*x)+(7*x)-6;
+printf("la valesur deu polynome est %d \n",P);
+    
+return 0;
+}
